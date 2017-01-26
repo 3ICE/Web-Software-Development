2254792f893b2a3932d1770f5ad9489510bbe58f
Daniel Berezvai <daniel.berezvai@student.tut.fi>
2017-01-27 00:43:21 +0200

    I might do this, actually

--- exercises/04_python/song.py
+++ exercises/04_python/song.py
@@ -1,1 +1,39 @@
- 
+class Song:
+    def __init__(self, x):
+        self._x = x
+
+    @property
+    def artist(self):
+        return "file"
+
+    @property
+    def title(self):
+        return "not found"
+
+    @property
+    def timestamp(self):
+        return "-1"
+
+    @property
+    def track_id(self):
+        return "given_id" # should be whatever track_id was given
+
+    @property
+    def tags(self):
+        return []
+
+    @property
+    def similars(self):
+        return []
+
+    def get_tags(self, limit=0):
+        return []
+
+    def get_similars(self, limit=0):
+        return []
+
+    def shared_tags(self, other_song_instance):
+        return ()
+
+    def combined_tags(self, other_song_instance):
+        return ()
