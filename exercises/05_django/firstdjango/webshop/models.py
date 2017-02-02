f0fc7cb88b221397695fbf1af26395bf1a6a8a00
Daniel Berezvai <daniel.berezvai@student.tut.fi>
2017-02-03 00:59:17 +0200

    models (3) task instructions were not explained well enough

--- exercises/05_django/firstdjango/webshop/models.py
+++ exercises/05_django/firstdjango/webshop/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+
 class Product(models.Model):
-    """
-    Write your model for the exercise 3 here. Remove the pass text.
-    """
-    pass
+    title = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    image_url = models.URLField(blank=True)
+    quantity = models.IntegerField(default=0)
+
+    def sell(self):
+        # decrease the quantity of the product by one.
+        self.quantity-=1
+        self.save()
+        return self.quantity
